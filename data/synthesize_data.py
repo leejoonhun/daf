f420061b880867fd09e66fbd0287c3d6c766f1bd
Lee Joonhun <jhlee63@outlook.com>
2023-01-19 17:30:36 +0900

    Equalize synthetic datasets with the paper

--- data/synthesize_data.py
+++ data/synthesize_data.py
@@ -11,21 +11,20 @@ def make_coldstart(
     data_num: Union[int, float] = 5e3,
     src_hist_len: int = 144,
     tgt_hist_lens: Tuple[int] = [36, 45, 54],
-    tgt_period: int = 36,
+    pred_len: int = 18,
 ):
     tgt_diversity = len(tgt_hist_lens)
     src_data, tgt_data = [], [[] for _ in range(tgt_diversity)]
     for _ in range(int(data_num)):
         src_data.append(
-            (
-                np.sin(
-                    np.arange(src_hist_len)
-                    * (2 * np.pi / np.random.randint(1, src_hist_len))
-                    + np.random.uniform(-np.pi, np.pi)
-                )
-                + np.random.uniform(-1, 1)
+            np.random.uniform(0.5, 5.0)
+            * np.sin(
+                np.arange(src_hist_len + pred_len)
+                * (2 * np.pi / np.random.uniform(144 / 20, 144))
+                + np.random.uniform(-2.0, 2.0) * np.pi
             )
-            * np.random.uniform(1, 1e3)
+            + np.random.uniform(-3.0, 3.0)
+            + np.random.normal(0, 0.2)
         )
     with open(DATA_ROOT / "coldstart_source.pkl", "wb") as f:
         pickle.dump(src_data, f)
@@ -34,15 +33,13 @@ def make_coldstart(
         tgt_hist_len = tgt_hist_lens[i]
         for _ in range(int(data_num)):
             tgt_data[i].append(
-                (
-                    np.sin(
-                        np.arange(tgt_hist_len)
-                        * (2 * np.pi / np.random.randint(1, tgt_hist_len))
-                        + np.random.uniform(-np.pi, np.pi)
-                    )
-                    + np.random.uniform(-1, 1)
+                np.random.uniform(0.5, 5.0)
+                * np.sin(
+                    np.arange(tgt_hist_len + pred_len) * (2 * np.pi / 36)
+                    + np.random.uniform(-2.0, 2.0) * np.pi
                 )
-                * np.random.uniform(1, 1e3)
+                + np.random.uniform(-3.0, 3.0)
+                + np.random.normal(0, 0.2)
             )
         with open(DATA_ROOT / f"coldstart_target_{tgt_hist_lens[i]}.pkl", "wb") as f:
             pickle.dump(tgt_data[i], f)
@@ -52,19 +49,20 @@ def make_fewshot(
     src_data_num: Union[int, float] = 5e3,
     tgt_data_nums: Tuple[int] = [20, 50, 100],
     hist_len: int = 144,
+    pred_len: int = 18,
 ):
     tgt_diversity = len(tgt_data_nums)
     src_data, tgt_data = [], [[] for _ in range(tgt_diversity)]
     for _ in range(int(src_data_num)):
         src_data.append(
-            (
-                np.sin(
-                    np.arange(hist_len) * (2 * np.pi / np.random.randint(1, hist_len))
-                    + np.random.uniform(-np.pi, np.pi)
-                )
-                + np.random.uniform(-1, 1)
+            np.random.uniform(0.5, 5.0)
+            * np.sin(
+                np.arange(hist_len + pred_len)
+                * (2 * np.pi / np.random.uniform(144 / 20, 144))
+                + np.random.uniform(-2.0, 2.0) * np.pi
             )
-            * np.random.uniform(1, 1e3)
+            + np.random.uniform(-3.0, 3.0)
+            + np.random.normal(0, 0.2)
         )
     with open(DATA_ROOT / "fewshot_source.pkl", "wb") as f:
         pickle.dump(src_data, f)
@@ -73,15 +71,14 @@ def make_fewshot(
         tgt_data_num = tgt_data_nums[i]
         for _ in range(int(tgt_data_num)):
             tgt_data[i].append(
-                (
-                    np.sin(
-                        np.arange(hist_len)
-                        * (2 * np.pi / np.random.randint(1, hist_len))
-                        + np.random.uniform(-np.pi, np.pi)
-                    )
-                    + np.random.uniform(-1, 1)
+                np.random.uniform(0.5, 5.0)
+                * np.sin(
+                    np.arange(hist_len + pred_len)
+                    * (2 * np.pi / np.random.uniform(144 / 20, 144))
+                    + np.random.uniform(-2.0, 2.0) * np.pi
                 )
-                * np.random.uniform(1, 1e3)
+                + np.random.uniform(-3.0, 3.0)
+                + np.random.normal(0, 0.2)
             )
         with open(DATA_ROOT / f"fewshot_target_{tgt_data_nums[i]}.pkl", "wb") as f:
             pickle.dump(tgt_data[i], f)
